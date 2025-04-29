1d27683a62663d18de4281ee3cfd9b192aa334ae
KoenVanBelle <koen.vanbelle@bignited.be>
2025-04-29 13:44:52 +0200

    Fix: remove unwanted cypress definition

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,34 +1,32 @@
-/// <reference types="cypress" />
-
 import { Course } from "../fixtures/course"
 import { createCourse } from "../pages/create-course"
 import { login } from "../pages/login"
 import { summary } from "../pages/summary"
 
 Cypress.Commands.add('login', (username: string, password: string) => {
-    cy.get(login.input.username).type(username)
-    cy.get(login.input.password).type(password)
-    cy.get(login.button.login).click()
+  cy.get(login.input.username).type(username)
+  cy.get(login.input.password).type(password)
+  cy.get(login.button.login).click()
 })
 
 Cypress.Commands.add('fillForm', (course: Course) => {
-    cy.get(createCourse.input.name).type(course.name)
-    cy.get(createCourse.input.description).type(course.description)
-    cy.get(createCourse.select.location).select(course.location)
-    cy.get(createCourse.input.teacher).type(course.teacher)
-    cy.get(createCourse.input.date).type(course.date)
-    cy.get(createCourse.input.startTime).type(course.timeStart)
-    cy.get(createCourse.input.endTime).type(course.timeEnd)
-    cy.get(createCourse.button.uploadImage).selectFile(course.image, {force : true})
-    cy.get(createCourse.input.phone).type(course.contactPhone)
-    cy.get(createCourse.input.email).type(course.contactEmail)
-    cy.get(createCourse.radio.type(course.type)).click()
-    
-    cy.get(createCourse.button.addCourse).click()
+  cy.get(createCourse.input.name).type(course.name)
+  cy.get(createCourse.input.description).type(course.description)
+  cy.get(createCourse.select.location).select(course.location)
+  cy.get(createCourse.input.teacher).type(course.teacher)
+  cy.get(createCourse.input.date).type(course.date)
+  cy.get(createCourse.input.startTime).type(course.timeStart)
+  cy.get(createCourse.input.endTime).type(course.timeEnd)
+  cy.get(createCourse.button.uploadImage).selectFile(course.image, { force: true })
+  cy.get(createCourse.input.phone).type(course.contactPhone)
+  cy.get(createCourse.input.email).type(course.contactEmail)
+  cy.get(createCourse.radio.type(course.type)).click()
+
+  cy.get(createCourse.button.addCourse).click()
 })
 
 
 Cypress.Commands.add('createCourse', (course: Course) => {
-    cy.fillForm(course)
-    cy.get(summary.button.approve).click()
+  cy.fillForm(course)
+  cy.get(summary.button.approve).click()
 })
